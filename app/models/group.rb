e4b57832ea32e73227fd914881320f1b4f966911
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-04-15 21:35:40 +0100

    validation test

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  validates :name, uninqueness: true
+  validates :name, uniqueness: true
   has_many :user_groups
   has_many :users, through: :user_groups
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    it 'validates uniquness' do
+      group = Group.create(name: 'some_group')
+      group_2 = Group.new(name: 'some_group')
+      group_2.valid?
+
+      expect(group_2.errors[:name]).to include("has already been taken")
+    end
+  end
 end
